43acb8144e9168b9e9f8c30a00436b7e872b6a7d
haithamhazaymeh <115452379+haithamhazaymeh@users.noreply.github.com>
2022-11-30 22:17:38 +0300

    calc30/11

---
+++ calculator.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="calculator.aspx.cs" Inherits="calculator.calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="number1"></asp:Label> 
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+            <asp:Label ID="Label2" runat="server" Text="number2"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+
+
+            <asp:Button ID="Button1" runat="server" Text="+" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" Text="-" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" runat="server" Text="*" OnClick="Button3_Click" />
+            <asp:Button ID="Button4" runat="server" Text="/" OnClick="Button4_Click" /> <br />
+
+            <asp:Label ID="Label3" runat="server" Text="Result"></asp:Label>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ calculator.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace calculator
+{
+    public partial class calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double num1=Convert.ToDouble(TextBox1.Text);
+            double num2=Convert.ToDouble(TextBox2.Text);
+            double sum= num1+ num2;
+            TextBox3.Text = sum.ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(TextBox1.Text);
+            double num2 = Convert.ToDouble(TextBox2.Text);
+            double sub = num1 - num2;
+            TextBox3.Text = sub.ToString();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(TextBox1.Text);
+            double num2 = Convert.ToDouble(TextBox2.Text);
+            double mul = num1 * num2;
+            TextBox3.Text = mul.ToString();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(TextBox1.Text);
+            double num2 = Convert.ToDouble(TextBox2.Text);
+            double dev = num1 / num2;
+            TextBox3.Text = dev.ToString();
+        }
+    }
+}
